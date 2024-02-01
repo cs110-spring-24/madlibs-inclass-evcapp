4ddc3b7bcd3e17f5dc50515ffb99def3a5944cbd
evcapp <157657958+evcapp@users.noreply.github.com>
2024-02-01 15:39:28 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,17 @@
 # TODO Create your inputs, variables, and add your story here!
+noun = input("Enter a noun: ")
+adjective= input("Enter a adjective: ")
+verb = input("Enter a verb: ") 
+
+name= input("Enter a name: ")
+adjective2= input("Enter a adjective: ")
+verb2= input("Enter a verb: ")
+name2= input("Enter a name: ")
+name3= input("Enter a name: ")
+place=input("Enter a place: ")
+verb3= input("Enter a verb: ")
+adjective3= input("Enter a adjective: ")
+adjective4= input("Enter a adjective: ")
+verb4= input("Enter a verb in the past tense: ")
+
+print(f" Once upon a time , {noun} was {adjective} while {verb} . {name} is very {adjective2} to {verb2} today. Afterwards, They meet {name3} at {place}. They all {verb3} very {adjective3}. Then the {adjective4} group {verb4}. The end) ")
